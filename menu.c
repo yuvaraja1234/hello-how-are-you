73094b03a526c175997efc305eb942cc0724f248
yuvaraja1234 <161583057+yuvaraja1234@users.noreply.github.com>
2024-02-28 15:15:36 +0530

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+void main(){
+
+    int a, i=1;
+
+    printf("Menu\n");
+
+    for (i;i<=4;i++)
+
+    {
+
+        printf("Option %d\n", i);
+
+    }
+
+    printf("Choose any one option: ");
+
+    scanf("%d",&a);
+
+    
+void fun(){
+
+        if (a==1){
+
+            printf("Hello\n");
+
+        }
+
+        else if (a==2){
+
+            printf("Hi\n");
+
+        }
+
+        else if (a==3){
+
+           printf("Bye\n");
+
+        }
+        else if (a==4){
+            exit(0);
+        }
+        else{
+            printf("Please select option displayed above: \n");
+            scanf("%d", &a);
+            fun(a);
+        }
+    }
+    fun(a);
+}
